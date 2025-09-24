4906b5074ff4cca6c8be5d56c7c16719b73279d1
stcollin <stcollin@ncsu.edu>
2025-09-23 21:24:42 -0400

    Add Prototype_5

---
+++ Prototype_5/Assets/Scripts/Target.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float minSpeed = 12f;
+    public float maxSpeed = 16f;
+
+    public float maxTorque = 10f;
+
+    public float xRange = 4f;
+    public float ySpawnPos = -6f;
+
+    private Rigidbody targetRb;
+    private AudioSource targetAudio;
+    private GameManager gameManager;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
+    public AudioClip clickSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        targetRb.AddForce(RandomUpwardForce(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        targetAudio = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    Vector3 RandomUpwardForce()
+    {
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        return Vector3.up * randomSpeed;
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        float randomXPos = Random.Range(-xRange, xRange);
+        return new Vector3(randomXPos, ySpawnPos);
+    }
+
+    float RandomTorque()
+    {
+        float randomTorque = Random.Range(0, maxTorque);
+        return randomTorque;
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            targetAudio.PlayOneShot(clickSound, 1.0f);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+}
